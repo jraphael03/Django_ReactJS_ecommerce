407ff0f21d483fd82db25b443e1f6b2321fb19e9
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-03-28 22:45:04 -0400

    Created reducers, constants, and actions for Products

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+
+// Instead of making Products API call from HomeScreen we will do it in this file
+
+const listProducts = () => async (dispatch) => {    // dispatch action to reducers, then update state
+    try{
+      dispatch({ type: PRODUCT_LIST_REQUEST })
+
+      // MAKE API CALL
+      const { data } = await axios.get("/api/products/")     // GRAB AND DESTRUCTURE DATA
+
+      dispatch({
+        type: PRODUCT_LIST_SUCCESS,
+        payload: data       // data we return from the API call
+      })
+        
+
+    } catch(error){
+        dispatch({
+          type: PRODUCT_LIST_FAIL,
+          payload: error.response && error.response.data.message    // If we received an error message
+          ? error.response.data.message     // Give the error message
+          : error.message,                  // If not display generic message
+        })
+
+    }
+}
---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,27 @@
+// Reducer is a function that takes in the current state, and will take in an action of what we want to do to the state
+// take in state and create an action that will return a new copy into the store
+
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+
+
+export const productListReducer = (state = { products: [] }, action) => {
+  // state is an empty array of products
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, product: [] }; // While loadig product array is still empty
+
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, product: action.payload }; // When finished loading, products is whatever data action was able to find
+
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }; // when finished if nothing was found give an error
+
+    default:
+      return state;             // If none of the above happen return back original state
+  }
+};
+
