ed5226153c31e2cd41853d734c5c9c847e8af770
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-04-01 13:16:32 -0400

    created view and url to get order from db

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -3,4 +3,5 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='orders-add'),    # http://127.0.0.1:8000/api/add/
+    path('<str:pk>/', views.getOrderById, name='user-order'),    # http://127.0.0.1:8000/api/<str:pk>/
 ]
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -60,3 +60,23 @@ def addOrderItems(request):
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
 
+
+# GET COMPLETED ORDER FROM THE DB
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])  
+def getOrderById(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+        # two types of users need to view this, customer, and admin
+        if user.is_staff or order.user == user:         # if is_staff (django's version of admin) or is the same user
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail':'Not authorized to view this order'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail': 'Order does not exist'},
+                            status = status.HTTP_400_BAD_REQUEST)
